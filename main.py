878af925abbd9e8590c535d1fa420679bae63ddb
Divrr <96827300+Divrr@users.noreply.github.com>
2024-07-25 21:34:13 +0800

    Restarting by using [disnake](https://docs.disnake.dev/) because it's more intuitive

--- main.py
+++ main.py
@@ -1,67 +1,18 @@
-import hikari
-import lightbulb
-import secret
-import time
+import disnake
+import logging
+import os
 
-bot = lightbulb.BotApp(token=secret.token())
+logging.basicConfig(level=logging.INFO)
+intents = disnake.Intents.all()
 
-class Timer:
-    def __init__(self):
-        self.timers = {}
+client = disnake.Client(intents=intents)
 
-    def start_timer(self, user_id):
-        if user_id not in self.timers:
-            self.timers[user_id] = time.monotonic()
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
 
-    def check_timer(self, user_id):
-        if user_id in self.timers:
-            time_elapsed = time.monotonic() - self.timers[user_id]
-            return time_elapsed
-        return None
+@client.event
+async def on_message(message):
+        print(f'Message from {message.author}: {message.content}')
 
-    def stop_timer(self, user_id):
-        if user_id in self.timers:
-            time_elapsed = time.monotonic() - self.timers[user_id]
-            del self.timers[user_id]
-            return time_elapsed
-        return None
-
-timer = Timer()
-
-@bot.command
-@lightbulb.command('timer', 'start/stop a personal timer')
-@lightbulb.implements(lightbulb.SlashCommandGroup)
-async def timer_command(ctx):
-    pass
-
-@timer_command.child
-@lightbulb.command('start', 'start timer')
-@lightbulb.implements(lightbulb.SlashSubCommand)
-async def start_timer(ctx):
-    """Starts the timer."""
-    timer.start_timer(ctx.author.id)
-    await ctx.respond("Timer started.")
-
-@timer_command.child
-@lightbulb.command('check', 'check timer')
-@lightbulb.implements(lightbulb.SlashSubCommand)
-async def check_timer(ctx):
-    """Stops the timer and reports the elapsed time."""
-    time_elapsed = timer.check_timer(ctx.author.id)
-    if time_elapsed is None:
-        await ctx.respond("Timer not started.")
-    else:
-        await ctx.respond(f"Time elapsed: {time_elapsed:.2f} seconds.")
-
-@timer_command.child
-@lightbulb.command('stop', 'stop timer')
-@lightbulb.implements(lightbulb.SlashSubCommand)
-async def stop_timer(ctx):
-    """Stops the timer and reports the elapsed time."""
-    time_elapsed = timer.stop_timer(ctx.author.id)
-    if time_elapsed is None:
-        await ctx.respond("Timer not started.")
-    else:
-        await ctx.respond(f"Time elapsed: {time_elapsed:.2f} seconds.")
-
-bot.run()
+client.run(os.getenv('DISCORD_TOKEN'))
